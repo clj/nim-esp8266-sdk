a18143bb936f6b3635bc94cf0631880d17d8022a
Christian Jacobsen <christian@lyderjacobsen.org>
2019-11-09 23:26:07 +0100

    Add helper functions to assign strings to array of uint8
    E.g. for setting up WiFi

--- nim/types.nim
+++ nim/types.nim
@@ -29,3 +29,23 @@ converter `cconststring->cstring`*(s: cconststring): cstring {.inline.} =
 converter `cstring->cconststring`*(s: cstring): cconststring {.inline.} =
   ## Implicit conversion from ``cconststring`` to ``cstring``.
   cast[cconststring](s)
+
+
+proc set_inner*(dst: var openarray[uint8], src: string) =
+  var i = 0
+  while i < len(src):
+    dst[i] = cast[uint8](src[i])
+    inc(i)
+  dst[i] = cast[uint8](0)
+
+
+template set_string*(dst: var openarray[uint8], src: string) =
+  set_inner(dst, src)
+
+
+template set_string*[T](dst: var array[T, uint8], src: static string) =
+  when len(src) == len(dst):
+    {.error: "value '" & src & "' too long, no space for null char in destination array of length " & $len(dst) .}
+  when len(src) > len(dst):
+    {.error: "length of value '" & src & "' (" & $len(src) & ") does not fit in destnation array of lenth: " & $len(dst) .}
+  set_inner(dst, src)
